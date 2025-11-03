a97453e432288c001cb1cc40fc1c79d51f8a14e5
AkshayaBhati <114259992+AkshayaBhati@users.noreply.github.com>
2025-11-02 23:07:27 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/tinyml-site/', // must match the repo name
+  base: '/',                 // custom domain needs root base
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    sourcemap: true
+  }
 })
 
